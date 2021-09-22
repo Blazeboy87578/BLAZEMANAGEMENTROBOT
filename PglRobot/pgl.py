7b4c23f121e331b05ad8bf3055d2f97380597ba3
Blazeboy87578 <91199102+Blazeboy87578@users.noreply.github.com>
2021-09-22 19:18:10 +0530

    Update pgl.py

--- PglRobot/pgl.py
+++ PglRobot/pgl.py
@@ -1,10 +1,10 @@
 from telethon import events
-from PglRobot import telethn
+from bLaZebot import telethn
 
 """Triggers start command in pm and in groupchats"""
 
 
-def PglRobot(**args):
+def bLaZebot(**args):
     """New message."""
     pattern = args.get("pattern", None)
     r_pattern = r"^[/!]"
